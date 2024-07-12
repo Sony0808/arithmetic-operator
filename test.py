66e1bc0016cec3e3d66d1b69a6c2419e9cc58fff
Sony0808 <sonysweety1000@gmail.com>
2024-07-12 11:24:35 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,15 @@
+print("Exam Resuilts")
+marks = 84
+if marks ==35:
+    print("you are promoted!")
+elif marks > 35:
+    print("you passed the exam!")
+if marks >=75 and marks <=85:
+    print("you got good marks:",marks)
+elif marks > 85:
+    print("you got great marks:",marks)
+elif marks > 95:
+    print("you got best marks:", marks)
+else:
+    print("you failed!",marks)
+
